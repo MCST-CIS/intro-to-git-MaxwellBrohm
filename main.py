0a6ed0426a57303c8eba4ebbdf49faa10c174da9
MaxwellBrohm <160036477+MaxwellBrohm@users.noreply.github.com>
2024-09-20 09:15:03 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+correctans = 0
+print("Welcome to Python Quiz Game!")
+print("Would you like to start the quiz? (Y/N)")
+answer = input("> ")
+if answer.lower() == 'y':
+    print("Question 1: What is Zander Leon's youtube/tiktok account?")
+    print("(A.) zandasalamanda")
+    print("(B.) zandywandy")
+    print("(C.) Ilovesalamanders123")
+    print("(D.) monkeytype4life")
+else:
+    print("Ok, bye!")
